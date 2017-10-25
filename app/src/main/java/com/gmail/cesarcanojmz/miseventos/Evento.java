089108c1ae08d9be9b112cd50c981b596240e002
Unknown <cesarcanojmz@gmail.com>
2017-10-25 16:14:44 -0500

    Modulo Agregar Evento v2 + BD v1

--- app/src/main/java/com/gmail/cesarcanojmz/miseventos/Evento.java
+++ app/src/main/java/com/gmail/cesarcanojmz/miseventos/Evento.java
@@ -1,6 +1,11 @@
 package com.gmail.cesarcanojmz.miseventos;
 
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
 import android.util.Log;
+import android.widget.Spinner;
+import android.widget.Toast;
+
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
@@ -13,21 +18,25 @@ public class Evento {
 
     private String nombre;
     private String descripcion;
-    private String diaSemana;
+    private String tipo;
+    private int numDiaSemana;
     private String hora;
     private String fecha;
+    private Context context;
 
-    public Evento(String nombre, String descripcion, int dia, int mes, int anio, int hora, int minuto) {
+    public Evento(String nombre, String descripcion, String tipo, int dia, int mes, int anio, int hora, int minuto, Context contexto) {
         this.nombre = nombre;
         this.descripcion = descripcion;
+        this.tipo = tipo;
         this.hora = setFormatHour(hora, minuto);
         this.fecha = setFormatDate(dia, mes, anio);
-        getDayOfWeek(dia, mes, anio);
+        this.context = contexto;
+        this.numDiaSemana =  getDayOfWeek(dia, mes, anio);
     }
 
     private String setFormatDate(int dia, int mes, int anio) {
         String formatfecha;
-        SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
         Date date = Calendar.getInstance().getTime();
         date.setDate(dia);
         date.setMonth(mes);
@@ -49,38 +58,53 @@ public class Evento {
         return formatHora;
     }
 
-    private void getDayOfWeek(int dia, int mes, int anio) {
+    private int getDayOfWeek(int dia, int mes, int anio) {
         String diaSemana;
+        int nDia = 0;
         SimpleDateFormat dateFormat = new SimpleDateFormat("EEE");
         Date date = Calendar.getInstance().getTime();
         date.setDate(dia);
         date.setMonth(mes);
         date.setYear(anio- 1900);
         diaSemana = dateFormat.format(date);
+
         switch (diaSemana) {
             case "dom.":
-                diaSemana = "domingo";
+                nDia = 7;
                 break;
             case "lun.":
-                diaSemana = "lunes";
+                nDia = 1;
                 break;
             case "mar.":
-                diaSemana = "martes";
+                nDia = 2;
                 break;
-            case "mie.":
-                diaSemana = "miercoles";
+            case "mié.":
+                nDia = 3;
                 break;
             case "jue.":
-                diaSemana = "jueves";
+                nDia = 4;
                 break;
             case "vie.":
-                diaSemana = "sabado";
+                nDia = 5;
                 break;
-            case "sab.":
-                diaSemana = "domingo";
+            case "sáb.":
+                nDia = 6;
                 break;
         }
-        Log.d("Dia de la semana ", diaSemana);
-        //return diaSemana;
+
+        Log.d("Dia de la semana ", diaSemana + " " + Integer.toString(nDia) );
+        return nDia;
     }
+
+    /**
+     * FUNCIONES PARA CONSULTAR/ACTUALIZAR LA BD
+     */
+
+    public void crearEvento() {
+        AdminSQLite dbHandler;
+        dbHandler= new AdminSQLite(this.context, null, null, 1);
+        SQLiteDatabase db = dbHandler.getWritableDatabase();
+        dbHandler.createEvento(this.nombre, this.descripcion, this.tipo, this.fecha, this.hora, this.numDiaSemana);
+    }
+
 }
